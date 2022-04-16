75a019c6ac779c6d74d8b8b08fb5a6626979b819
yssjtu <635664803@qq.com>
2022-04-16 17:42:06 +0800

    Update run_gen_prefix.py

--- sh/run_gen_prefix.py
+++ sh/run_gen_prefix.py
@@ -178,14 +178,13 @@ def print_params(model):
     Trainable_params = 0
     NonTrainable_params = 0
 
-    # 遍历model.parameters()返回的全局参数列表
     for param in model.parameters():
-        mulValue = np.prod(param.size())  # 使用numpy prod接口计算参数数组所有元素之积
-        Total_params += mulValue  # 总参数量
+        mulValue = np.prod(param.size())  
+        Total_params += mulValue  
         if param.requires_grad:
-            Trainable_params += mulValue  # 可训练参数量
+            Trainable_params += mulValue  
         else:
-            NonTrainable_params += mulValue  # 非可训练参数量
+            NonTrainable_params += mulValue  
 
     print(f'Total params: {Total_params}')
     print(f'Trainable params: {Trainable_params}')
